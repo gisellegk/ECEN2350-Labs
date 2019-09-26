81effaa0675884c8a576219e77fdffdb348aed42
Giselle <gisellegk@gmail.com>
2019-09-26 14:30:11 -0600

    it work

--- lab1_block2/twoscompvalue.v
+++ lab1_block2/twoscompvalue.v
@@ -3,13 +3,12 @@ module twoscompval(
     output reg [3:0] SBINARY
 );
 
-reg PH;
+
 always @ (TWOSCOMP[3:0])
     begin
         if(TWOSCOMP[3])
             begin
-                PH = ~TWOSCOMP;
-                SBINARY = PH + 1;
+                SBINARY = ~TWOSCOMP + 1'b1;
             end
         else 
             SBINARY = TWOSCOMP;
