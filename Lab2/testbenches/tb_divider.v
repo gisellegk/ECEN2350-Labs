73187cb0f2f67cb7ceaf366b17f809ed340e767b
PSCurlin <phaedra.curlin@outlook.fr>
2019-10-26 18:40:05 -0600

    -Added comments to divider and tb_divider

--- Lab2/testbenches/tb_divider.v
+++ Lab2/testbenches/tb_divider.v
@@ -1,3 +1,9 @@
+/* 
+*   Test bench for the clock divider.
+*   Generates clock signal of 10ns in period.
+*   Resets at the first full cycle's negetive edge
+*/
+
 `timescale 1 ns / 100 ps
 module tb_divider();
     reg clock,reset_n,div_clock;
--- Lab2/verilog/divider.v
+++ Lab2/verilog/divider.v
@@ -1,3 +1,11 @@
+/* 
+   ECEN 2350: Lab 2
+   Module divides an inputted clock by 2.  
+   Use reset_n on a negative edge of the clock signal to divide the clock.
+   Holds clock value otherwise.
+*/
+
+
 module divider(input clock,input reset_n,output reg div_clock);
 
 always @(posedge clock)  //Detects for positive edge of clock 
