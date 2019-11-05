db3f92f7cb7d24abe428d670e40c7a1709cc813d
Giselle <gisellegk@gmail.com>
2019-11-04 20:23:33 -0700

    syntax errors, mostly stuff just needs to be reg. also leap year would get overwritten by the next if statement. still does not fully work.

--- Lab2/verilog/dateconverter.v
+++ Lab2/verilog/dateconverter.v
@@ -6,28 +6,41 @@
 
 module dateconverter(input [6:0]date, input leapYear, output [1:0]month, output [3:0]dayOnes, output [1:0]dayTens);
 // leap by default is 0 for no leap year. Implement without it for now. 
-@always()
+reg [6:0]date_reg;
+
+//assign outputs
+reg [1:0]month_reg;
+assign month = month_reg;
+reg [3:0]dayOnes_reg;
+assign dayOnes = dayOnes_reg;
+reg [1:0]dayTens_reg;
+assign dayTens = dayTens_reg;
+
+wire leapDay = (date == 20) & leapYear;
+
+always@(date)
 begin
+    date_reg = date;
 	if(leapYear) begin
 		if(date>60)
-			date = date-1;
-		else if(date == 60)begin
-			month = 2;
-			dayOnes = 9;
-			dayTens = 2;
-		end
+			date_reg = date_reg-1;
 	end
-	if (date <=31)
-		month = 1;
-	else if (31 < date && date <= 59) begin
-		month = 2;
-		date = date-31;
+    if(leapDay) begin
+		month_reg = 2;
+		dayOnes_reg = 9;
+		dayTens_reg = 2;
+	    end
+	else if (date_reg <=31)
+		month_reg = 1;
+	else if (31 < date_reg && date_reg <= 59) begin
+		month_reg = 2;
+		date_reg = date_reg-31;
 		end
-	else if (59 < date)begin
-		month = 3;
-		date = date - 59;
+	else if (59 < date_reg) begin
+		month_reg = 3;
+		date_reg = date_reg - 59;
 	end
-	dayOnes == date%10;
-	dayTens == date/10;
+	dayOnes_reg = date%10;
+	dayTens_reg = date/10;
 end
 endmodule
