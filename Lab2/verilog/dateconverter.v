620392f6c179c5515fb0ec71d1e94da107c2a875
Braedon Bellamy <braedonb98@gmail.com>
2019-11-04 18:53:14 -0700

    Updating date converter

--- Lab2/verilog/dateconverter.v
+++ Lab2/verilog/dateconverter.v
@@ -4,8 +4,30 @@
 */
 
 
-module dateconverter(input [6:0]num, input leap, output [1:0]month, output [3:0]day_1s, output [1:0]day_10s);
+module dateconverter(input [6:0]date, input leapYear, output [1:0]month, output [3:0]dayOnes, output [1:0]dayTens);
 // leap by default is 0 for no leap year. Implement without it for now. 
-
-
+@always()
+begin
+	if(leapYear) begin
+		if(date>60)
+			date = date-1;
+		else if(date == 60)begin
+			month = 2;
+			dayOnes = 9;
+			dayTens = 2;
+		end
+	end
+	if (date <=31)
+		month = 1;
+	else if (31 < date && date <= 59) begin
+		month = 2;
+		date = date-31;
+		end
+	else if (59 < date)begin
+		month = 3;
+		date = date - 59;
+	end
+	dayOnes == date%10;
+	dayTens == date/10;
+end
 endmodule
